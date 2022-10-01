bc6fa912d58cf5baa438d490f51513a2b3597262
Teagan Durtschi <teagan@durtschi.com>
2022-10-01 11:42:13 -0500

    Next version

---
+++ assets/PageLayout.vue_vue_type_style_index_0_lang.3e3af8f2.js
@@ -0,0 +1,1 @@
+import{d as s,b as a,a as t,r as o,o as n}from"./index.d562dd7f.js";const _={class:"page-layout"},c={class:"content"},r={class:"about"},u=s({__name:"PageLayout",setup(l){return(e,d)=>(n(),a("div",_,[t("div",c,[o(e.$slots,"content")]),t("div",r,[o(e.$slots,"about")])]))}});export{u as _};
